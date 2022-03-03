ab31e7990f0c56ecf2e22bbbce8959684a491173
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2022-03-03 20:05:11 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import configparser #imports configparser, an module that makes it possiable to read config files
+import configparser
 import os
-import subprocess #imports subprocess, which makes it possiable to do system commands
-
-config = configparser.ConfigParser()#sets the name config to = configparser
-config.read("config.ini")#reads the config file
-IP = config["IP"]#sets the row IP to = IP 
-print(list(IP))#prints avablibe ips on the IP row
-IN=input("Which computer would you like to connect to| ")#ask the user what computer do they want to connect to and sets the users in put to "IN"
-ssh=(IP[IN])#sets the name ssh to = IP{the row ip} and IN {the users input}
-subprocess.run(["ssh", ssh]) # runs ssh with the verable ssh {the set ip}
+import subprocess 
+config = configparser.ConfigParser()
+config.read("config.ini")
+IP = config["IP"]
+print(list(IP))
+IN=input("Which computer would you like to connect to| ")
+ssh=(IP[IN])
+subprocess.run(["ssh", ssh])
