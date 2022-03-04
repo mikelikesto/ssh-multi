dcca736ed0676230eb311dafb740863dda401667
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2022-03-04 13:36:48 +1100

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-subprocess.run(["gcc sshm.c -o sshm"])
-subprocess.run(["sudo mv sshm /bin"])
-subprocess.run(["mv ssh-multi.py /home/$USER"])
-subprocess.run(["mv config.py /home/$USER"])
+subprocess.run(["gcc sshm.c -o sshm && sudo mv sshm /bin && mv ssh-multi.py /home/$USER && mv config.py /home/$USER"])
+#subprocess.run(["sudo mv sshm /bin"])
+#subprocess.run(["mv ssh-multi.py /home/$USER"])
+#subprocess.run(["mv config.py /home/$USER"])
