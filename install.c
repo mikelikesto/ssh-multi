e404ef5f52734a94e8fb87c9e86dc9d4d5dcea4e
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2022-03-04 17:30:40 +1100

    Update and rename install.py to install.c

---
+++ install.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+   char command[50];
+
+   strcpy( command, "gcc sshm.c -o sshm && sudo mv sshm /bin && mv ssh-multi.py /home/$USER && mv config.py /home/$USER" );
+   system(command);
+
+   return(0);
+} 
--- install.py
+++
@@ -1,6 +0,0 @@
-import os
-import subprocess
-subprocess.run(["gcc sshm.c -o sshm && sudo mv sshm /bin && mv ssh-multi.py /home/$USER && mv config.py /home/$USER"])
-#subprocess.run(["sudo mv sshm /bin"])
-#subprocess.run(["mv ssh-multi.py /home/$USER"])
-#subprocess.run(["mv config.py /home/$USER"])
