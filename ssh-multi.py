15946598ec3fb8a42ec9ce74748461f77ce74bfa
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2023-06-15 14:31:58 +1000

    Update ssh-multi.py

--- ssh-multi.py
+++ ssh-multi.py
@@ -3,7 +3,7 @@ import os
 import subprocess 
 config = configparser.ConfigParser()
 config.read("config.ini")
-IP = config["IP"]
+IP = config.center["IP"]
 subprocess.run(["clear"])
 print(list(IP))
 IN=input("Which computer would you like to connect to| ")
